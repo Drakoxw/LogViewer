e966a16ebe7dd4eab8c588a8d78c859023b3e3d8
Sergio Salazar <desarrollo4@aveonline.co>
2024-05-22 08:57:57 -0500

    Ajustar url final para errores MIM

--- src/app/core/constants/common.ts
+++ src/app/core/constants/common.ts
@@ -6,7 +6,7 @@ export const CLICK = {
 }
 
 export const URL_API_BASE = 'https://api.aveonline.co/api-logistics/public/api'
-export const URL_API_MIM_ERRORES = 'http://localhost:8084/ila/public/api/apiflujos/errors'
+export const URL_API_MIM_ERRORES = 'https://api.aveonline.co/ila/public/api/apiflujos/errors'
 // export const URL_API_MIM_ERRORES = 'https://app.apiflujos.com/api/v1/aveonline/errores/'
 
 export const URL_APP_OLD = 'https://app.aveonline.co/app/logs'
